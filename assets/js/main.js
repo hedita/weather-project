e9d3dded0f90f1506741d37058cbbe697bdc950c
Hedita <hedi.takrami@gmail.com>
2022-09-10 10:35:56 +0430

    add name of the city

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const cityName = document.getElementById("city-name");
+
+
+async function getWeather() {
+    const result = await fetch("http://api.openweathermap.org/geo/1.0/reverse?lat=37.2713&lon=49.5921&limit=&exclude=daily&appid=9249d4649089f661a90f4514cc165a33")
+    const resultJson = await result.json();
+    const [{name}] = resultJson;
+
+    cityName.innerHTML = name;
+}
+  getWeather(); 
