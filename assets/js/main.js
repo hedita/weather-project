864663b3af604960c0e03d2caf16e2d18f5fa467
Hedita <hedi.takrami@gmail.com>
2022-09-15 12:41:11 +0430

    add date

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,19 @@
-const cityName = document.getElementById("city-name");
-
-
 async function getWeather() {
-    const result = await fetch("http://api.openweathermap.org/geo/1.0/reverse?lat=37.2713&lon=49.5921&limit=&exclude=daily,weather.icon&appid=9249d4649089f661a90f4514cc165a33")
+    const result = await fetch("https://api.openweathermap.org/data/2.5/forecast?lat=37.2713&lon=49.5921&appid=e0cb24360c821c3571a49c9d05be0fb1")
     const resultJson = await result.json();
-    const [{name}] = resultJson;
-  console.log(resultJson)
-    cityName.innerHTML = name;
+    const cityName = document.getElementById("city-name");
+    //const {city: name} = resultJson;
+    //cityName.innerHTML = {cityName};
+    //console.log(resultJson);
 }
   getWeather(); 
+
+  const time = document.getElementById("time");
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.toLocaleString('default', { month: 'short' });
+  const dayName = now.toLocaleString('en-us', {weekday:'long'});
+  const dayNumber = now.getUTCDate();
+  const date = dayName + ", " + dayNumber + " " + month + " " + year;
+  time.innerHTML = date;
+
