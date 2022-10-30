f8866950d078b6993284a92a10b80967357d1a9c
Hedita <hedi.takrami@gmail.com>
2022-10-30 16:48:22 +0330

    add a variable for appid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+const appId = "e0cb24360c821c3571a49c9d05be0fb1";
 const selectedCityName = document.getElementById("cities");
 const lat = 37.2793607;
 const lon = 49.5846102;
@@ -15,7 +16,7 @@ selectedCityName.addEventListener("click", () => {
 
 async function getCityLocatin(city) {
   const fetchedData = await fetch(
-    `https://api.openweathermap.org/geo/1.0/direct?q=${city.name},${city.countryCode}&limit=1&appid=e0cb24360c821c3571a49c9d05be0fb1`
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city.name},${city.countryCode}&limit=1&appid=${appId}`
   );
   const selectedCityLocation = await fetchedData.json();
 
@@ -26,7 +27,7 @@ async function getCityLocatin(city) {
 
 async function getWeather(lat, lon) {
   const result = await fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=e0cb24360c821c3571a49c9d05be0fb1`
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${appId}`
   );
   const { city, list } = await result.json();
   const cityName = document.getElementById("city-name");
